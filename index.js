6ee586e0d937aed9b4718f599c20d9cdd818eb8b
Ermek <ermak_niceman@mail.ru>
2023-03-10 18:30:24 +0600

    Server listner

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function you(b,c) {
-    let a = b + c
-}
+const Koa = require('koa');
+const app = new Koa();
 
-new function {
-    
-}
+app.use(async (ctx) => {
+    ctx.body = 'Hello, world!';
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });

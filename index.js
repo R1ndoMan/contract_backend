724c8982185dc72c422773c5aa5ade6c154201de
Ermek <ermak_niceman@mail.ru>
2023-03-13 17:46:01 +0600

    add ПРИВЕТ МИР!

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ async function syncDatabase() {
   console.log('All models were synchronized successfully.');
 }
 
+app.use(async (ctx) => {
+  ctx.body = 'Привет, мир! и привет Ермек!';
+});
+
 async function startServer() {
   const app = new Koa();
   // ... здесь настраиваем приложение ...

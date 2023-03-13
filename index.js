d3108ae39bc9bf6b8c1a40a117baded168d1685f
Ermek <ermak_niceman@mail.ru>
2023-03-13 17:47:14 +0600

    fix

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ async function syncDatabase() {
   console.log('All models were synchronized successfully.');
 }
 
-app.use(async (ctx) => {
-  ctx.body = 'Привет, мир! и привет Ермек!';
-});
+
 
 async function startServer() {
   const app = new Koa();
+
+  app.use(async (ctx) => {
+    ctx.body = 'Привет, мир! и привет Ермек!';
+  });
   // ... здесь настраиваем приложение ...
   await syncDatabase();
   app.listen(3000, () => {

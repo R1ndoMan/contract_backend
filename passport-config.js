4afc0f16888d0df4dfad0be9456759b2f1eac255
Ermek <ermak_niceman@mail.ru>
2023-03-16 18:20:08 +0600

    fix

---
+++ passport-config.js
@@ -0,0 +1,44 @@
+const passport = require('koa-passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+}, async (email, password, done) => {
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return done(null, false, { message: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (!isPasswordValid) {
+      return done(null, false, { message: 'Invalid email or password' });
+    }
+
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});

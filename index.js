3f029a5d23f5c64d82c4644875628b8c137eeba3
Ermek <ermak_niceman@mail.ru>
2023-03-16 11:07:04 +0600

    fix

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ async function startServer() {
   await User.sync();
   console.log('All models were synchronized successfully.');
 
-  app.listen(2000, () => {
-    console.log('Server started on port 2000');
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
   });
 }
 
